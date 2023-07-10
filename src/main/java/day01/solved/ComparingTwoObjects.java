336b795e88b4feb77df4e761ec053a8cf72273ad
Tamilselvan2747 <116268684+Tamilselvan2747@users.noreply.github.com>
2023-07-10 17:05:45 +0530

    update on day01 solved

--- src/main/java/day01/solved/ComparingTwoObjects.java
+++ src/main/java/day01/solved/ComparingTwoObjects.java
@@ -1,19 +1,28 @@
 package day01.solved;
 
-public class TestBookComparison {
+/**
+ * Solved Example for day 1
+ * 
+ * @author BharathwajSoundarara
+ *
+ */
+class Movie {
+	public String title;
+
+}
+
+public class ComparingTwoObjects {
 	public static void main(String[] args) {
 		Movie movie1 = new Movie();
 		movie1.title = "Harry Potter";
-		
+
 		Movie movie2 = new Movie();
-		
+
 		movie2.title = "Lord of the Rings";
-		
+
 		// Each object will be Stored in a separate memory location
 		// so it will print false
 		System.out.println(movie1 == movie2);
-		
-		
-		
+
 	}
 }
